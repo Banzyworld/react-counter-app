45e121df9ba20a9a762b7a466b2e94a89f2e3df1
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-08-01 15:36:17 +0100

    remodified color & position of "count"

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Button (props){
 
-    let {action, title} = props;
+    let { action, title} = props;
     return( <button onClick={action}>{title}</button>
     
        
