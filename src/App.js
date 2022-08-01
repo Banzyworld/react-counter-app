eb9bfce7bd9e622d13cd7c154e8ee8b2c1fc895f
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-08-01 15:18:43 +0100

    modified source folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+import Button from "./Button";
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+  const resetCount = () => {
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' >
+      <div>
+        <div className='count'>
+          <h3>Counter App</h3>
+          <h1>{count}</h1>
+        </div>
+        <div className='buttons'>
+          <Button title={"Decrement"} action={decrementCount}/>
+          <Button title={"Increment"} action={incrementCount}/>
+          <Button title={"reset"} action={resetCount}/>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function Button (props){
+
+    let {action, title} = props;
+    return( <button onClick={action}>{title}</button>
+    
+       
+    )
+}
